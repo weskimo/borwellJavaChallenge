0bf4143be1ccebf51c376982251349064f772f72
Weskimo <samuel.hughes4@btinternet.com>
2022-05-20 21:55:34 +0100

    fields and constructors made for the 2 classes

--- borwellChallenge/src/lib/CylinderRoom.java
+++ borwellChallenge/src/lib/CylinderRoom.java
@@ -1,23 +1,20 @@
 package lib;
 
-public class RectangularTankRoom {
+public class CylinderRoom {
 	// wall paramas for input
-    private double floorLength;
-    private double floorWidth;
+    private double floorRadius;
     private double wallHeight;
     private double totalWindowArea;
     
-    // stores the value of how much area 1 litre of paint will cover.
+    
     private double wallAreaSqMetrePerPaint;
     
-    // constructor: Inputs: floor radius in metres, wall height in metres, 
-    // total window area in m^2
-    public RectangularTankRoom(double floorLength, double floorWidth, double wallHeight, 
+    // constructor: Inputs: floor radius in metres, wall height in metres, total window area in m^2 
+    public CylinderRoom(double floorRadius, double wallHeight, 
     		double totalWindowArea) {
         
     	// set the custom dimensions of the room
-        this.floorLength = floorLength;
-        this.floorWidth = floorWidth;
+        this.floorRadius = floorRadius;
         this.wallHeight = wallHeight;
         this.totalWindowArea = totalWindowArea;
         
